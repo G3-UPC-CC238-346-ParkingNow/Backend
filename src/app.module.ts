d4c260b3b7d8a6cf3bef0644668c732391b1fe65
Samuel Molina <samuelmolina281816@gmail.com>
2025-05-16 00:33:15 -0500

    fix(app) Adding Services and Controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioController } from './usuario/usuario.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioService } from './usuario/usuario.service';
+import { LocalService } from './local/local.service';
+import { ReservaService } from './reserva/reserva.service';
+import { getMetadataArgsStorage } from 'typeorm';
+import { LocalController } from './local/local.controller';
+import { ReservaController } from './reserva/reserva.controller';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'parkingnow',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature(
+      getMetadataArgsStorage().tables.map((tbl) => tbl.target as any),
+    ),
+  ],
+  controllers: [AppController, UsuarioController, LocalController, ReservaController],
+  providers: [AppService, UsuarioService, LocalService, ReservaService],
 })
 export class AppModule {}
